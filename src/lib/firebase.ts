e41c3ff27411abbb7f107aad28ddca08219a185d
Fiifi Michaels <michaelquaicoe60@gmail.com>
2025-08-20 02:54:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, initializeFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -25,7 +25,10 @@ if (!getApps().length) {
     app = getApp();
 }
 
-const db = getFirestore(app);
+// Use initializeFirestore to enable experimental features
+const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
 const auth = getAuth(app);
 
 export { db, auth };
