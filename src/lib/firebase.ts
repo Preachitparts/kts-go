cb99870819ea30b572b7c704dfc5fbc3717ceaa4
Fiifi Michaels <michaelquaicoe60@gmail.com>
2025-08-27 07:17:47 +0000

    I can't access any data in it.  Even admin logins, I'm told incorrect cr

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore, initializeFirestore } from "firebase/firestore";
@@ -8,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyANdr4m7IuVgvyPEJwrp-Ix9CaxyArBW5M",
-  authDomain: "kts-go.firebaseapp.com",
-  projectId: "kts-go",
-  storageBucket: "kts-go.appspot.com",
-  messagingSenderId: "733494640668",
-  appId: "1:733494640668:web:62ca8eb3202c28a1d59fd9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 
